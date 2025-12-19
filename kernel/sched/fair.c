cfed39bfeb8e674ae7287317c5159d486fb674f7
Flopster101 <nahuelgomez329@gmail.com>
2025-12-19 14:55:11 -0300

    sched: fair: Increase min granularity to 2ms
    Reduce overhead in context switching.
    
    Signed-off-by: Flopster101 <nahuelgomez329@gmail.com>
    Signed-off-by: dupazlasu <dupazlasu66@gmail.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -61,9 +61,9 @@ enum sched_tunable_scaling sysctl_sched_tunable_scaling = SCHED_TUNABLESCALING_L
  *
  * (default: 0.75 msec * (1 + ilog(ncpus)), units: nanoseconds)
  */
-unsigned int sysctl_sched_min_granularity			= 750000ULL;
+unsigned int sysctl_sched_min_granularity			= 2000000ULL;
 EXPORT_SYMBOL_GPL(sysctl_sched_min_granularity);
-static unsigned int normalized_sysctl_sched_min_granularity	= 750000ULL;
+static unsigned int normalized_sysctl_sched_min_granularity	= 2000000ULL;
 
 /*
  * This value is kept at sysctl_sched_latency/sysctl_sched_min_granularity
